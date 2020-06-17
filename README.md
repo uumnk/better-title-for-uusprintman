cc58ff0d2e388e7e984de324c33820d90922cb1f
uumnk <56532513+uumnk@users.noreply.github.com>
2020-06-17 08:49:02 +0200

    First version of Better title for uuSprintman

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # better-title-for-uusprintman
 Adds page name to uuSprintman's document title to make it better and make saving to bookmarks easier.
+
+**Quick guide:**
+1. Put better-title-for-uusprintman.user.js into Tampermonkey addon for Firefox and turn it on.
+2. (Re)load some uuSprintman page with Ticket.
+
+**Known issues: note that in current version:**
+1. The document title is altered only in tickets (tasks).
+2. The document title is set only once after loading uuSprintman, if you go to another ticket, you have to refresh the page to change the document title.
---
+++ better-title-for-uusprintman.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         Better title for uuSprintman
+// @namespace    https://github.com/uumnk/better-title-for-uusprintman
+// @version      0.1
+// @description  Adds page name to uuSprintman's document title to make it better and make saving to bookmarks easier.
+// @author       Monika
+// @match        https://uuapp.plus4u.net/uu-sprintman-maing01/*/ticket?id=*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const CHANGE_TITLE_ORDER = true;
+    let PAGE_TITLE_REGEX = /(^.+)-(.{4})-(.+)/;
+
+    // Page content is loaded dynamically so we need to wait and try it in intervals (body.onload is triggered before the content is loaded so it won't help).
+    const interval = 1000;
+    // There is limited count of attempts because structure of the page could change so this script would be looking for it forever.
+    let attempts = 10;
+    // Query was made to ensure that the page title is foud and nothing else. If the script stops working, maybe structure of the page has changed and it will be necessary to change this query.
+    let titleQuery = ".uu5-bricks-page-content-body>.uusprintman-routes-ticket>.uu5-common-div>.uusprintman-core-ticket-detail>.uu5-common-div>.uusprintman-core-ticket-detail-section>h1.uu5-bricks-header>.uu5-common-div>.uu5-bricks-section>h2.uu5-bricks-header>.uu5-common-div>.uu5-common-div>.uup-bricks-infoicon-header>.uu5-bricks-text"
+
+    let timer = setInterval(() => {
+        let documentTitle = document.title;
+        let titleElementList = document.querySelectorAll(titleQuery);
+        if (documentTitle === "uuSprintman" && titleElementList.length === 1) { // Document title is unchanged and the page title element was found.
+            let pageTitle = titleElementList[0].innerHTML;
+            if (CHANGE_TITLE_ORDER && PAGE_TITLE_REGEX.test(pageTitle)) {
+                pageTitle = pageTitle.replace(PAGE_TITLE_REGEX, '$2-$3 ($1)');
+            }
+            document.title = pageTitle + " - " + documentTitle;
+            clearInterval(timer);
+        } else if (attempts > 0) { // Page content is probably not loaded yet.
+            attempts--;
+        } else { // Page title was probably not found (or title was changed or the loading is incredibly slow).
+            clearInterval(timer);
+        }
+    }, interval);
+})();
